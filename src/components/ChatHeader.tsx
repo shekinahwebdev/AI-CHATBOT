339b0f91b02055412f0bf9524f199e384bfa5b09
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-07-28 18:39:19 +0000

    Enhance ChatHeader component with new layout and button; update title and remove unused tsconfig option

--- src/components/ChatHeader.tsx
+++ src/components/ChatHeader.tsx
@@ -1,10 +1,18 @@
 import chatbot from "/assets/chatbot-3.png";
-
+import arrowDown from "/assets/caret-down.png";
 export const ChatHeader = () => {
   return (
-    <div className="chatbot_header-icon">
-      <img src={chatbot} alt="Chatbot Icon" className="chatbot_icon" />
-      <h1 className="chatbot_title">AI Chatbot</h1>
+    <div className="chatbot_header">
+      <div className="chatbot_header_line">
+        <div className="chatbot_icon">
+          <img src={chatbot} alt="Chatbot Icon" className="chatbot_icon" />
+        </div>
+        <h1 className="chatbot_title">YumGenie Chatbot</h1>
+      </div>
+
+      <button className="chatbot_button">
+        <img src={arrowDown} alt="Arrow Down" className="chatbot_arrow" />
+      </button>
     </div>
   );
 };
