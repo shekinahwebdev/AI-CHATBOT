8ec30dc4927ddcdf3c0356ee6a15bc1557f99536
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-08-07 21:40:33 +0000

    Implement user and bot interaction

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -10,16 +10,18 @@ interface ChatMessageProps {
 }
 
 export const ChatMessage: React.FC<ChatMessageProps> = ({ message }) => {
-  return (
-    <div className={`${message.role === "bot" ? "bot" : "user"}_message`}>
-      {message.role === "bot" && (
-        <div className="chatbot_message">
-          <div className="chatbot_icon-container">
-            <img src={chatbot} alt="Chatbot Icon" className="chatbot_icon" />
-          </div>
-          <p className="message_text">{message.content}</p>
+  if (message.role === "bot") {
+    return (
+      <div className="chatbot_message">
+        <div className="chatbot_icon-container">
+          <img src={chatbot} alt="Chatbot Icon" className="chatbot_icon" />
         </div>
-      )}
+        <p className="message_text">{message.content}</p>
+      </div>
+    );
+  }
+  return (
+    <div className="user_message">
       <p className="message_text">{message.content}</p>
     </div>
   );
